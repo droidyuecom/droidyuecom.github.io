399a91f09aa891c83c9bd8c599323de4c1f2fdd9
droidyuecom <40063888+droidyuecom@users.noreply.github.com>
2018-07-26 21:43:50 +0800

    Create qr_images.js

---
+++ javascript/qr_images.js
@@ -0,0 +1,10 @@
+var images = ["https://asset.droidyue.com/image/kotlin_zhangtao.jpeg",
+						"https://asset.droidyue.com/image/qutan_wangluo_xieyi.png",
+						"https://asset.droidyue.com/image/ruanjianceshi.jpeg",
+						"https://asset.droidyue.com/image/java_basic.jpeg",
+						"https://asset.droidyue.com/image/zhuyun_jishu_guanli.jpeg",
+						"https://asset.droidyue.com/image/gitchat_refer.jpeg",
+						"https://asset.droidyue.com/image/machine_learning.jpeg"]
+		for (var i = 0; i < images.length; i++) {
+			document.write("<img class='no_boarder_class' style='max-Width:100%' src='" + images[i] + "'>");
+		};
