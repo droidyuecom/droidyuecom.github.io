26ed07921ce7828480d6ffd5244b695056afd3b4
droidyuecom <40063888+droidyuecom@users.noreply.github.com>
2018-08-02 14:30:35 +0800

    Update qr_images.js

--- javascript/qr_images.js
+++ javascript/qr_images.js
@@ -1,4 +1,6 @@
 var images = [
+	"https://asset.droidyue.com/image/zuoerduohaozi.jpeg",
+	"https://asset.droidyue.com/image/jishulingdaoli.jpeg",
 	"https://asset.droidyue.com/image/java_vm.jpeg",
 	"https://asset.droidyue.com/image/kotlin_zhangtao.jpeg",
 						"https://asset.droidyue.com/image/qutan_wangluo_xieyi.png",
