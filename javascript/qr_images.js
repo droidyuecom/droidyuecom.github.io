091e45d4f87749f265140846ca56ba6c987b8ba3
droidyuecom <40063888+droidyuecom@users.noreply.github.com>
2018-12-15 20:48:09 +0800

    Update qr_images.js

--- javascript/qr_images.js
+++ javascript/qr_images.js
@@ -1,12 +1,28 @@
 var images = [
-	"https://asset.droidyue.com/image/zuoerduohaozi.jpeg",
-	"https://asset.droidyue.com/image/jishulingdaoli.jpeg",
-	"https://asset.droidyue.com/image/java_vm.jpeg",
-	"https://asset.droidyue.com/image/kotlin_zhangtao.jpeg",
-						"https://asset.droidyue.com/image/qutan_wangluo_xieyi.png",
-						"https://asset.droidyue.com/image/java_basic.jpeg",
-						"https://asset.droidyue.com/image/gitchat_refer.jpeg",
-	     ]
+	"https://asset.droidyue.com/image/maths_for_programer.jpeg",
+	
+	"https://asset.droidyue.com/image/suanfa_1212.jpeg",  
+	
+	"https://asset.droidyue.com/image/geektime/tech_management.jpeg",
+	
+	"https://asset.droidyue.com/image/geektime/qiuyue_de_chanpin_shijian.jpeg",
+	
+	"https://asset.droidyue.com/image/geektime/shenru_chajie_jvm.jpeg",
+	
+	"https://asset.droidyue.com/image/geektime/kuaishu_shangshou_kotlin_kaifa_zhangtao.jpeg",
+	
+	"https://asset.droidyue.com/image/geektime/qutan_wangluo_xieyi_1212.jpeg",
+	
+	"https://asset.droidyue.com/image/geektime/java_hexin_jishu_36_jiang_1212.jpeg",
+	
+	"https://asset.droidyue.com/image/geektime/zhuyun_de_jishu_guanlike_1212.jpeg",
+	
+	"https://asset.droidyue.com/image/geektime/zuoer_tingfeng.jpeg",
+	
+	"https://asset.droidyue.com/image/geektime/jishulingdaoli_300_jiang_1212.jpeg" 
+
+
+	]
 		for (var i = 0; i < images.length; i++) {
 			document.write("<img class='no_boarder_class' style='max-Width:100%' src='" + images[i] + "'>");
 		};
