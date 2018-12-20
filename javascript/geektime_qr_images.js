258b6009865712539b9dcb376230b99f81a01881
Duan Jianhua <duanjianhua@secoo.com>
2018-12-20 19:58:20 +0800

    modified:   javascript/geektime_qr_images.js

--- javascript/geektime_qr_images.js
+++ javascript/geektime_qr_images.js
@@ -1,26 +1,25 @@
 var geekTimeQrImages = [
 	"https://asset.droidyue.com/image/maths_for_programer.jpeg",
-	
-	"https://asset.droidyue.com/image/suanfa_1212.jpeg",  
-	
-	"https://asset.droidyue.com/image/geektime/tech_management.jpeg",
-	
-	"https://asset.droidyue.com/image/geektime/qiuyue_de_chanpin_shijian.jpeg",
-	
-	"https://asset.droidyue.com/image/geektime/shenru_chajie_jvm.jpeg",
-	
-	"https://asset.droidyue.com/image/geektime/kuaishu_shangshou_kotlin_kaifa_zhangtao.jpeg",
-	
-	"https://asset.droidyue.com/image/geektime/qutan_wangluo_xieyi_1212.jpeg",
-	
-	"https://asset.droidyue.com/image/geektime/java_hexin_jishu_36_jiang_1212.jpeg",
-	
-	"https://asset.droidyue.com/image/geektime/zhuyun_de_jishu_guanlike_1212.jpeg",
-	
-	"https://asset.droidyue.com/image/geektime/zuoer_tingfeng.jpeg",
-	
-	"https://asset.droidyue.com/image/geektime/jishulingdaoli_300_jiang_1212.jpeg" 
 
+	"https://asset.droidyue.com/image/geektime/suanfa_mianshi_tongguan_40_jiang.jpeg",
+
+	"https://asset.droidyue.com/image/geektime/tech_management.jpeg",
+
+	"https://asset.droidyue.com/image/geektime/qiuyue_de_chanpin_shijian.jpeg",
+
+	"https://asset.droidyue.com/image/geektime/shenru_chajie_jvm.jpeg",
+
+	"https://asset.droidyue.com/image/geektime/kuaishu_shangshou_kotlin_kaifa_zhangtao.jpeg",
+
+    "https://asset.droidyue.com/image/geektime/qutanwangluoxieyi.jpeg",
+
+    "https://asset.droidyue.com/image/geektime/java_hexin_jishu_36_jiang_1224.jpeg",
+
+    "https://asset.droidyue.com/image/geektime/zhuyun_de_jishu_guanli_ke.jpeg",
+
+	"https://asset.droidyue.com/image/geektime/zuoer_tingfeng.jpeg",
+
+    "https://asset.droidyue.com/image/geektime/jishu_lingdaoli_300_jiang_1224.jpeg"
 
 ];
 
