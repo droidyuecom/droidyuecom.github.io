2cc6bbdf6c0c2d894990acd2e2c333c55feef0dd
droidyuecom <40063888+droidyuecom@users.noreply.github.com>
2019-03-08 17:36:19 +0800

    Update geektime_qr_images.js

--- javascript/geektime_qr_images.js
+++ javascript/geektime_qr_images.js
@@ -11,17 +11,21 @@ var geekTimeQrImages = [
 
 	"https://asset.droidyue.com/image/geektime/kuaishu_shangshou_kotlin_kaifa_zhangtao.jpeg",
 
-    "https://asset.droidyue.com/image/geektime/qutanwangluoxieyi.jpeg",
+    	"https://asset.droidyue.com/image/geektime/qutanwangluoxieyi.jpeg",
 
-    "https://asset.droidyue.com/image/java_hexin_jishu_36_jiang.jpeg",
+    	"https://asset.droidyue.com/image/java_hexin_jishu_36_jiang.jpeg",
 
-    "https://asset.droidyue.com/image/geektime/zhuyun_de_jishu_guanli_ke.jpeg",
+    	"https://asset.droidyue.com/image/geektime/zhuyun_de_jishu_guanli_ke.jpeg",
 
 	"https://asset.droidyue.com/image/geektime/zuoerduo_haozi_zhuanlan.jpeg",
 
-    "https://asset.droidyue.com/image/jishlingdaoli_300_jiang.jpeg",
+    	"https://asset.droidyue.com/image/jishlingdaoli_300_jiang.jpeg",
+	
 	"https://asset.droidyue.com/image/geektime/mysql_shizhan_0308.png",
-	"https://asset.droidyue.com/image/geektime/go_yuyan_hexin_36_jiang.png"
+	
+	"https://asset.droidyue.com/image/geektime/go_yuyan_hexin_36_jiang.png",
+	
+	"https://asset.droidyue.com/image/geektime/jiqixuexi_40_jiang_0308.png"
 
 ];
 
