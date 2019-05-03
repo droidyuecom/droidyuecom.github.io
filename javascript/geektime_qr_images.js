6e39a2f315f17465f4278155de477e0245c81b08
droidyuecom <40063888+droidyuecom@users.noreply.github.com>
2019-05-03 14:40:30 +0800

    Update geektime_qr_images.js

--- javascript/geektime_qr_images.js
+++ javascript/geektime_qr_images.js
@@ -27,8 +27,7 @@ var geekTimeQrImages = [
 	
 	"https://asset.droidyue.com/image/geektime/jiqixuexi_40_jiang_0308.png",
 	
-	"https://asset.droidyue.com/image/2019_05/gitchat_refer.jpeg"
-
+	"https://asset.droidyue.com/image/2019_05/gitchat_refer_new.jpg"
 ];
 
 geekTimeQrImages = shuffleArray(geekTimeQrImages);
